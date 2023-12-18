885bd84fc07a597fd60ea032264e16258694072d
beluga2810 <voitovitchegorushka@yandex.by>
2023-12-19 01:58:47 +0300

    adding search

--- app/src/components/tours/tours.jsx
+++ app/src/components/tours/tours.jsx
@@ -58,12 +58,77 @@ const Tours = () =>{
                           },
                     ]}/>
                 </div>
-                <div className={style.filter_search}>
-                    Поиск
-                </div>
             </div>
             <div className={style.content}>
+                <div className={style.card}>
+                  <div className={style.abs}>
+                    <div>Откуда?: Могилёв</div>
+                    <div>Куда?: Могилёв</div>
+                    <div>300 Br</div>
+                  </div>
+                  <div className={style.abs_bottom}>
+                    <div>Подробнее</div>
+                    <div>Купить</div>
+                  </div>
+                    <div className={style.image}>
 
+                    </div>
+                    <div className={style.info}>
+                      <div className={style.tittle}>
+                        <h1 className={style.name}>Экскурсия по минскому вокзалу</h1>
+                        <p>
+                          Ultimate Guide to Visiting Harry Potter Studio Tour London by Warner Bros in Leavesden. 
+                          An absolute must visit for Harry Potter fans in London! #whatshotblog #harrypotter #londontravel #harrypotterfan
+                        </p>
+                      </div>
+                    </div>
+                </div>
+                <div className={style.card}>
+                  <div className={style.abs}>
+                    <div>Откуда?: Могилёв</div>
+                    <div>Куда?: Могилёв</div>
+                    <div>300 Br</div>
+                  </div>
+                  <div className={style.abs_bottom}>
+                    <div>Подробнее</div>
+                    <div>Купить</div>
+                  </div>
+                    <div className={style.image}>
+
+                    </div>
+                    <div className={style.info}>
+                      <div className={style.tittle}>
+                        <h1 className={style.name}>Экскурсия по минскому вокзалу</h1>
+                        <p>
+                          Ultimate Guide to Visiting Harry Potter Studio Tour London by Warner Bros in Leavesden. 
+                          An absolute must visit for Harry Potter fans in London! #whatshotblog #harrypotter #londontravel #harrypotterfan
+                        </p>
+                      </div>
+                    </div>
+                </div>
+                <div className={style.card}>
+                  <div className={style.abs}>
+                    <div>Откуда?: Могилёв</div>
+                    <div>Куда?: Могилёв</div>
+                    <div>300 Br</div>
+                  </div>
+                  <div className={style.abs_bottom}>
+                    <div>Подробнее</div>
+                    <div>Купить</div>
+                  </div>
+                    <div className={style.image}>
+
+                    </div>
+                    <div className={style.info}>
+                      <div className={style.tittle}>
+                        <h1 className={style.name}>Экскурсия по минскому вокзалу</h1>
+                        <p>
+                          Ultimate Guide to Visiting Harry Potter Studio Tour London by Warner Bros in Leavesden. 
+                          An absolute must visit for Harry Potter fans in London! #whatshotblog #harrypotter #londontravel #harrypotterfan
+                        </p>
+                      </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
